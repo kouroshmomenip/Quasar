ec0bcda69c1d62fadc16e69248840b095abe0ca9
kourosh momeni <kouroshmomeni@kouroshs-MacBook-Pro.local>
2024-11-28 17:56:54 +0330

    your-comment

---
+++ src/pages/auth/LoginPage.vue
@@ -0,0 +1,71 @@
+<template>
+    <q-page-container>
+      <q-page class="window-height window-width row justify-center items-center auth-background">
+        <q-card
+                style="border-left: 3px solid black; border-bottom: 3px solid black;" class="q-pa-lg w-400">
+            <div class="text-center q-mb-md">
+                <h5 class="custom-title">Welcome to Our site!</h5>
+            </div>
+            <q-card-section>
+          <q-form class="q-gutter-md">
+            <q-input
+              v-model="loginParameters.username"
+              square
+              filled
+              clearable
+              type="email"
+              label="Email"
+              class="input-modern"
+            />
+            <q-input
+              v-model="loginParameters.password"
+              square
+              filled
+              clearable
+              type="password"
+              label="Password"
+              class="input-modern"
+            />
+          </q-form>
+        </q-card-section>
+        <q-card-actions class="q-px-md">
+          <q-btn
+            @click="login()"
+            unelevated
+            color="primary"
+            size="lg"
+            class="full-width modern-btn"
+            label="Login"
+          />
+          <q-btn
+            @click="register()"
+            unelevated
+            color="secondary"
+            size="lg"
+            class="full-width modern-btn q-mt-sm"
+            label="Register"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-page>
+  </q-page-container>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const loginParameters = ref({
+  username: '',
+  password: '',
+});
+
+const login = () => {
+  console.log(loginParameters.value.username, loginParameters.value.password);
+};
+
+const router = useRouter();
+const register = () => {
+  router.replace({ name: 'register' });
+};
+</script>
---
+++ src/pages/auth/RegisterPage.vue
@@ -0,0 +1,95 @@
+<template>
+    <q-page-container>
+      <q-page class="window-height window-width row justify-center items-center auth-background">
+        <q-card
+                style="border-left: 3px solid black; border-bottom: 3px solid black;" class="q-pa-lg w-400">
+                <div class="text-center q-mb-md">
+                <h5 class="custom-title">Welcome to Our site!</h5>
+            </div>
+          <q-card-section>
+            <q-form class="q-gutter-md">
+              <q-input
+                v-model="registerParameters.name"
+                square
+                filled
+                clearable
+                type="text"
+                label="Full Name"
+                class="input-modern"
+              />
+              <q-input
+                v-model="registerParameters.email"
+                square
+                filled
+                clearable
+                type="email"
+                label="Email"
+                class="input-modern"
+              />
+              <q-input
+                v-model="registerParameters.password"
+                square
+                filled
+                clearable
+                type="password"
+                label="Password"
+                class="input-modern"
+              />
+              <q-file
+                v-model="registerParameters.avatar"
+                ref="avatarState"
+                filled
+                label="Upload Avatar"
+                class="input-modern"
+              />
+            </q-form>
+          </q-card-section>
+          <q-card-actions class="q-px-md">
+            <q-btn
+              @click="register()"
+              unelevated
+              color="primary"
+              size="lg"
+              class="full-width modern-btn"
+              label="Register"
+            />
+            <q-btn
+              @click="login()"
+              unelevated
+              color="secondary"
+              size="lg"
+              class="full-width modern-btn q-mt-sm"
+              label="Login"
+            />
+          </q-card-actions>
+        </q-card>
+      </q-page>
+    </q-page-container>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
+  
+  const registerParameters = ref({
+    name: '',
+    email: '',
+    password: '',
+    avatar: [],
+  });
+  
+  const register = () => {
+    console.log(
+      registerParameters.value.name,
+      registerParameters.value.email,
+      registerParameters.value.password,
+      registerParameters.value.avatar
+    );
+  };
+  
+  const router = useRouter();
+  const login = () => {
+    router.replace({ name: 'login' });
+  };
+  </script>
+  
